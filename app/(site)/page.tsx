e4f13e3c080d74f16936d7e00ac1e10017fbfbd9
Neel <nishxpatel@gmail.com>
2024-05-31 18:27:53 -0400

    some db stuff

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -34,10 +34,24 @@ export default function Home() {
               mt-4
             "
           >
-            <ListItem />
+            <ListItem 
+              image="/images/liked.jpg"
+              name="Liked Songs"
+              href="liked"
+            />
           </div>
         </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">
+            Newest Songs
+          </h1>
+        </div>
+        <div>
+          List of Songs!
+        </div>
+      </div>
     </div>
   );
 }
